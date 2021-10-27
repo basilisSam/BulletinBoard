150c3ace11a45db16357ebec781e1abc3422437f
Sam <Sam@DESKTOP-2IHO4FC>
2021-10-27 20:29:43 +0300

    fetching announcements by categoryID

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,19 @@
 import { useEffect, useState } from "react";
+import Announcement from "../announcement/Announcement";
 import Board from "../board/Board";
-
+import "./Home.css";
 const Home = () => {
   const [categories, setCategories] = useState([]);
+  const [ansemester1, setAnsemester1] = useState([]);
+  const [ansemester2, setAnsemester2] = useState([]);
+  const [ansemester3, setAnsemester3] = useState([]);
+
   // const [announcement, setAnnouncement] = useState([]);
 
   const Url = "http://localhost:8000/Categories";
+  const Urlan1 = "http://localhost:8000/Announcements?categoryId=0";
+  const Urlan2 = "http://localhost:8000/Announcements?categoryId=1";
+  const Urlan3 = "http://localhost:8000/Announcements?categoryId=2";
 
   useEffect(() => {
     fetch(Url)
@@ -23,10 +31,61 @@ const Home = () => {
         console.log("fetching columns failed :( ");
         console.log(e);
       });
+
+    fetch(Urlan1)
+      .then((announcements) => {
+        if (announcements.ok) {
+          return announcements.json();
+        }
+
+        throw announcements;
+      })
+      .then((announcements) => {
+        console.log(announcements, "semester1");
+        setAnsemester1(announcements);
+      })
+      .catch((e) => {
+        console.log("fetching columns failed :( ");
+        console.log(e);
+      });
+
+    fetch(Urlan2)
+      .then((announcements) => {
+        if (announcements.ok) {
+          return announcements.json();
+        }
+
+        throw announcements;
+      })
+      .then((announcements) => {
+        console.log(announcements, "semester2");
+        setAnsemester2(announcements);
+      })
+      .catch((e) => {
+        console.log("fetching columns failed :( ");
+        console.log(e);
+      });
+
+    fetch(Urlan3)
+      .then((announcements) => {
+        if (announcements.ok) {
+          return announcements.json();
+        }
+
+        throw announcements;
+      })
+      .then((announcements) => {
+        console.log(announcements, "semester3");
+        setAnsemester3(announcements);
+      })
+      .catch((e) => {
+        console.log("fetching columns failed :( ");
+        console.log(e);
+      });
   }, []);
 
   return (
-    <div>
+    <div className='homeWrapper'>
       <Board categories={categories} />
     </div>
   );
