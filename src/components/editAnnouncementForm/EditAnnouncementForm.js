567b4ad4d085437538a6a3efd017ca57496435f3
Sam <Sam@DESKTOP-2IHO4FC>
2021-11-22 20:11:32 +0200

    add style to EditAnnouncementForm

--- src/components/editAnnouncementForm/EditAnnouncementForm.js
+++ src/components/editAnnouncementForm/EditAnnouncementForm.js
@@ -1,3 +1,4 @@
+import "./EditAnnouncementForm.css"
 const EditAnnouncementForm = ({
   setIsEditing,
   handleEditTitleChange,
@@ -8,42 +9,97 @@ const EditAnnouncementForm = ({
   categories,
 }) => {
   return (
-    <div>
-      <form onSubmit={(e) => handleEditFormSubmit(e)}>
-        <label>
-          Title:
+    <form onSubmit={(e) => handleEditFormSubmit(e)}>
+      <div className='editAnnouncementTitleWrapper'>
+        <div className='editAnnouncementTitle'>
+          <label className='editAnnouncementTitleLabel' htmlFor='grid-title'>
+            Title:
+          </label>
           <input
-            onChange={(e) => handleEditTitleChange(e)}
+            className='editAnnouncementTitleInput'
+            id='grid-title'
+            required
             type='text'
             placeholder='Update a title...'
+            onChange={(e) => handleEditTitleChange(e)}
             value={announcementToBeEdited.title}
-            required
           />
-        </label>
-        <label>
-          Description
+          <p className='editAnnouncementTitleParagraph'>
+            Please fill out this field.
+          </p>
+        </div>
+      </div>
+      <div className='editAnnouncementTextWrapper'>
+        <div className='editAnnouncementText'>
+          <label
+            className='editAnnouncementTextLabel'
+            htmlFor='grid-description'
+          >
+            Description:
+          </label>
           <textarea
-            onChange={(e) => handleEditTextChange(e)}
-            placeholder='Write an Announcement'
-            value={announcementToBeEdited.text}
+            className='editAnnouncementTextInput'
+            id='grid-description'
             required
+            type='text'
+            placeholder='Edit an Announcement'
+            onChange={(e) => handleEditTextChange(e)}
+            value={announcementToBeEdited.text}
           />
-        </label>
-        <select onChange={(e) => handleEditCategory(e)}>
-          {categories.map((category) => (
-            <option defaultValue={announcementToBeEdited.category} key={category.id} value={category.id}>
-              {category.title}
-            </option>
-          ))}
-        </select>
+          <p className='editAnnouncementTextParagraph'>
+            Please fill out this field.
+          </p>
+        </div>
+      </div>
+      <div className='editAnnouncementStateWrapper'>
+        <div className='editAnnouncementState'>
+          <label className='editAnnouncementStateLabel' htmlFor='grid-state'>
+            State
+          </label>
+          <div className='editAnnouncementStateInputWrapper'>
+            <select
+              onChange={(e) => handleEditCategory(e)}
+              className='editAnnouncementStateSelect'
+              id='grid-state'
+            >
+              {" "}
+              {categories.map((category) => (
+                <option
+                  defaultValue={announcementToBeEdited.category}
+                  key={category.id}
+                  value={category.id}
+                >
+                  {category.title}
+                </option>
+              ))}
+            </select>
+            <div className='editAnnouncementStateSvgWrapper'>
+              <svg
+                className='editAnnouncementStateSvg'
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 20 20'
+              >
+                <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className=' editAnnouncementButtonWrapper'>
         <input
-          onClick={(e) => handleEditFormSubmit(e)}
           type='submit'
           value='Update Announcement'
+          className='editAnnouncementCreateButton'
+          onClick={(e) => handleEditFormSubmit(e)}
         />
-        <button onClick={() => setIsEditing(false)}>Cancel</button>
-      </form>
-    </div>
+        <button
+          className='editAnnouncementCancelButton'
+          onClick={() => setIsEditing(false)}
+        >
+          Cancel
+        </button>
+      </div>
+    </form>
   );
 };
 
